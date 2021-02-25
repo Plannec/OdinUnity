2e1db1dc449267b608cfea9db2240a4c451471ec
Nicolas Klier <klier.nicolas@protonmail.com>
2021-02-25 19:01:01 +0100

    Add a few effect on death event.

--- Assets/Scenes/PlayerMovement.cs
+++ Assets/Scenes/PlayerMovement.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Vector3 touchPosition;
+
+    private SpriteRenderer sprite;
     private Rigidbody2D rb;
     private BoxCollider2D bc;
+    private AudioSource audioSource;
+    private ParticleSystem dieParticles;
+    private HardLight2D light;
+
     private Vector3 direction;
     private float moveSpeed = 10f;
     private bool allowInput = true;
@@ -14,8 +20,12 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        sprite = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         bc = GetComponent<BoxCollider2D>();
+        audioSource = GetComponent<AudioSource>();
+        dieParticles = GetComponentInChildren<ParticleSystem>();
+        light = GetComponentInChildren<HardLight2D>();
     }
 
     // Update is called once per frame
@@ -34,19 +44,25 @@ public class PlayerMovement : MonoBehaviour
                 rb.velocity = Vector2.zero;
             }
         }
-
-      
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // We don't want to trigger when touching a wall.
         if(collision.gameObject.tag == "Wall")
         {
             return;
         }
 
+        // Apply a little force indicating death.
+        rb.velocity = new Vector2(0, 3);
+
+        audioSource.Play();
         rb.gravityScale = 1;
         allowInput = false;
         bc.isTrigger = false;
+        sprite.material.SetVector("_EmissionColor", Color.red);
+        light.Color = Color.Lerp(light.Color, Color.red, 2.0f);
+        dieParticles.Play();
     }
 }
